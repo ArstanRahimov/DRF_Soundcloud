226b81771d880179fa8c6b36e840fc5e2b8ecaae
Arstan <rahimov.arstan@gmail.com>
2022-01-23 21:36:45 +0600

    project setup, custom user model added

---
+++ oauth/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import AuthUser, SocialLink
+
+
+@admin.register(AuthUser)
+class AuthUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'display_name', 'join_date')
+    list_display_links = ('email',)
+
+
+@admin.register(SocialLink)
+class SocialLinkAdmin(admin.ModelAdmin):
+    list_display = ('user', 'link')
